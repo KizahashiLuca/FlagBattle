cebe3b171b920b8345222ad7cc17f22df4d939ba
KizahashiLuca <kizahashi.luca@gmail.com>
2018-04-26 10:03:58 +0900

    first commit

---
+++ src/flagbattle/plugin/FlagBattle.java
@@ -0,0 +1,31 @@
+package flagbattle.plugin;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FlagBattle extends JavaPlugin implements CommandExecutor {
+
+	@Override
+	public void onDisable() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onDisable();
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onEnable();
+
+		getCommand("startgame").setExecutor(new StartCommand());
+		getCommand("stopgame").setExecutor(new StopCommand());
+		getCommand("maketeam").setExecutor(new MakeTeamCommand());
+		getCommand("removeteam").setExecutor(new RemoveTeamCommand());
+		getCommand("leadteam").setExecutor(new LeadTeamCommand());
+		getCommand("addteammember").setExecutor(new AddTeamMemberCommand());
+		getCommand("rejectteammember").setExecutor(new RejectTeamMemberCommand());
+		getCommand("listteam").setExecutor(new ListTeamCommand());
+		getCommand("statusteam").setExecutor(new StatusTeamCommand());
+
+	}
+
+}
---
+++ src/flagbattle/plugin/StopCommand.java
@@ -0,0 +1,15 @@
+package flagbattle.plugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class StopCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String lavel, String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+}
